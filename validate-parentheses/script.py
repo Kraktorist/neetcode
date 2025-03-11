ffc44ea9add36da0f01811af27343d65a54fe08e
kraktorist <1@1.ru>
2025-03-11 21:56:51 +0300

    [validate-parentheses] complete

--- validate-parentheses/script.py
+++ validate-parentheses/script.py
@@ -1,3 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
+        stack = list()
+        pairs = {
+            '}' : '{',
+            ')' : '(',
+            ']' : '['
+        }
+        for i in range(len(s)):
+            letter = s[i]
+            if letter in '({[':
+                stack.append(letter)
+            elif len(stack):
+                if stack.pop() != pairs[letter]:
+                    return False
+            else:
+                return False
+        return not len(stack)
